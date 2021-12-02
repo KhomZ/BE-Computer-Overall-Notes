aec575f87334bf216bc1d305762c79a0e12d58a7
Khom <mgrprinc37@gmail.com>
2021-12-02 17:17:21 +0545

    ikhom: lets code on IPPR

---
+++ BE Computer/Semester/Sem-VII/Image-Processing-n-Pattern-Recognition/IPPR-Lab/readimage.py
@@ -0,0 +1,18 @@
+# Khom 
+# 29th November 2021
+
+import matplotlib.pyplot as plt
+from numpy import _ShapeType 
+
+flower = plt.imread('C:\Users\ACER\Desktop\IPPR\Lab1\Flowers\flower.jpg')
+
+
+print(flower)
+
+plt.imshow(flower)
+plt.show()
+
+
+# create one histogram array to find out the no. of zeros and 1s and then plot
+m,n = img, shape 
+r1, count1 = hist_plot(img)
