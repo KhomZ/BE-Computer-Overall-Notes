2f8ce768c22616adaf270cd878dcf69569d87e3d
Khom <mgrprinc37@gmail.com>
2021-12-04 16:58:25 +0545

    ikhom: solved Tower of Hanoi Problem using Python

---
+++ BE Computer/Semester/Sem-I/Programming in C (CMP 113)/References/Source Codes/hello-khom.cpp
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main() {
+   // printf() displays the string inside quotation
+   printf("Hello, Khom!");
+   return 0;
+}
---
+++ BE Computer/Semester/Sem-III/Data Structure and Algorithms (CMP 225)/References/Source Codes/tower-of-hanoi.py
@@ -0,0 +1,60 @@
+# @author : Khom
+
+# Python Program to solve the Tower of Hanoi.
+# Recursive Python function to solve the tower of hanoi
+
+# def TowerOfHanoi(n, source, destination, auxiliary):
+#     if n == 1:
+#         print("Move disk 1 from source", source, "to destination", destination)
+#         return
+
+#     else:
+#         TowerOfHanoi(n-1, source, auxiliary, destination)
+#         print("Move disk",n,"from source", source, "to destination", destination)
+#         TowerOfHanoi(n-1, auxiliary, destination, source)
+
+# # Driver Code
+# # n = 4
+# n = int(input('Pick a number for Tower of Hanoi!'))
+# TowerOfHanoi(n,'A','B','C')
+# # A, C, B are the name of rods
+
+
+
+# # outputs
+# for n = 4
+# Move disk 4 from source A to destination B
+# Move disk 1 from source C to destination B
+# Move disk 2 from source C to destination A
+# Move disk 1 from source B to destination A
+# Move disk 3 from source C to destination B
+# Move disk 1 from source A to destination C
+# Move disk 2 from source A to destination B
+# Move disk 1 from source C to destination B
+
+
+
+
+# refer: https://www.geeksforgeeks.org/python-program-for-tower-of-hanoi/
+
+
+
+
+
+# Creating a recursive function
+def tower_of_hanoi(disks, source, auxiliary, target):
+    if(disks == 1):
+        
+        print('Move disk 1 from rod {} to rod {}.'.format(source, target))
+
+        return
+
+    # function call itself
+    tower_of_hanoi(disks - 1, source, target, auxiliary)
+    print('Move disk {} from rod {} to rod {}.'.format(disks, source, target))
+    tower_of_hanoi(disks - 1, auxiliary, source, target)
+
+disks = int(input('Enter the number of disks:'))
+# We are referring source as A, auxiliary as B, and target as C.
+
+tower_of_hanoi(disks, 'A', 'B', 'C')  # Calling the function
